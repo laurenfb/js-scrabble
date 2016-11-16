58fb010286a2fa80f34ad28b18ffb0f524758fca
laurenfb <laurenfriesbrink@gmail.com>
2016-11-16 15:44:11 -0800

    new file for player module yay

---
+++ player.js
@@ -0,0 +1,9 @@
+var CONSTANTS = require('./scrabble_constants');
+var Scrabble = require('./scrabble')
+
+// constructor!
+var Player = function() {};
+
+
+// export this file as a module
+module.exports = Player;
