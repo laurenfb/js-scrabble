3c8dbb9e73e2d89f8bed1a3e3db3566086d6bb12
laurenfb <laurenfriesbrink@gmail.com>
2016-11-16 14:39:00 -0800

    finished score function

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,34 @@
+var CONSTANTS = require('./scrabble_constants');
+
 var Scrabble = function() {};
 
-// YOUR CODE HERE
+// this is a thing to copy from
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
 
+// add score method to Scrabble
+Scrabble.prototype.score = function(word){
+  word = word.toUpperCase();
+  total = 0;
+  for (let char of word) {
+    total += CONSTANTS.SCORES[char]
+  };
+  if (word.length >= 7) {
+    total += 50
+  };
+  return total;
+}
+
+
+// testing for score functions
+var scrabble = new Scrabble()
+
+// regular word
+console.log("Score for 'word' is: " + scrabble.score("word"))
+// bingo word (7+ letters)
+console.log("Score for 'foooooooblah' is: " + scrabble.score("foooooooblah"))
+
+
+// export this file as a module
 module.exports = Scrabble;
--- scrabble_constants.js
+++ scrabble_constants.js
@@ -1,6 +1,6 @@
 
-var ScrabbleConstants = {
-  SCORE_HASH: {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J:8,
+var SCRABBLE_CONSTANTS = {
+  SCORES: {A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J:8,
       K: 5, L:1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4,
       W: 4, X: 8, Y: 4, Z: 10},
   LETTERS:   [["A", "A", "A", "A", "A", "A", "A", "A", "A"],
@@ -32,6 +32,4 @@ var ScrabbleConstants = {
   MAX_TILES: 7
 }
 
-console.log(ScrabbleConstants.SCORE_HASH)
-
-module.exports = ScrabbleConstants
+module.exports = SCRABBLE_CONSTANTS
