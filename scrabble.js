355305d02752aed6206b01dc7a5acb4ad8682590
laurenfb <laurenfriesbrink@gmail.com>
2016-11-16 15:03:24 -0800

    added highestScoreFrom method, though it is still pretty minimal

--- scrabble.js
+++ scrabble.js
@@ -10,7 +10,7 @@ Scrabble.prototype.helloWorld = function() {
 // add score method to Scrabble
 Scrabble.prototype.score = function(word){
   word = word.toUpperCase();
-  total = 0;
+  var total = 0;
   for (let char of word) {
     total += CONSTANTS.SCORES[char]
   };
@@ -20,14 +20,37 @@ Scrabble.prototype.score = function(word){
   return total;
 }
 
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  var highestWord = null;
+  var highestWordScore = null;
+  var scoreArray = [];
 
-// testing for score functions
+// iterate through words, score them all
+  for (let word of arrayOfWords) {
+    scoreArray.push(Scrabble.prototype.score(word));
+  };
+
+// iterate through scores, find best based on conditional
+  for (var i = 0; i < scoreArray.length; i++) {
+    if (scoreArray[i] > highestWordScore) {
+      highestWordScore = scoreArray[i]
+      highestWord = arrayOfWords[i];
+    }
+  };
+  return highestWord
+};
+
+// new scrabble
 var scrabble = new Scrabble()
 
-// regular word
+// testing for score function// regular word
 console.log("Score for 'word' is: " + scrabble.score("word"))
 // bingo word (7+ letters)
 console.log("Score for 'foooooooblah' is: " + scrabble.score("foooooooblah"))
+
+console.log("hiii: " + scrabble.score("hiii") + ", bloop: " + scrabble.score("bloop") + ", cat: " + scrabble.score("cat") )
+// testing highestScoreFrom fx
+console.log("highest score word is: " + scrabble.highestScoreFrom(["hiii", "bloop", "cat"]))
 
 
 // export this file as a module
