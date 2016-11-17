ea8acb6f8967f33997de9208c0209f0d7ccca9e3
laurenfb <laurenfriesbrink@gmail.com>
2016-11-16 20:01:40 -0800

    totalScore fx

--- player.js
+++ player.js
@@ -17,21 +17,34 @@ Player.prototype = {
       this._plays.push(word);
       return this._plays;
     };
-  } //end of play fx
-
+  }, //end of play fx
+  totalScore(){
+    var score = this._score
+    this._plays.forEach(function(play){
+      score += Scrabble.score(play);
+    });
+    this._score = score;
+    return this._score;
+  } //end of totalScore fx
 }
 
 // testing that we can instantiate a new Player
 var player = new Player("Edith");
-console.log("Player is: " + player._name);
-console.log(player._name +  "'s plays: " + player._plays);
-console.log(player._name +  "'s score: " + player._score);
-console.log("Player won? " + player._won)
+// test attributes
+// console.log("Player is: " + player._name);
+// console.log(player._name +  "'s plays: " + player._plays);
+// console.log(player._name +  "'s score: " + player._score);
+// console.log("Player won? " + player._won)
+// test whether we can play stuff
 player.play("cat")
 console.log(player._name +  "'s plays:" + player._plays);
+// test ability to set whether player has won
 // player._won = true;
 // console.log(player._won);
 player.play("bloooop");
 console.log(player._name +  "'s plays:" + player._plays);
+// test totalScore fx
+console.log("Total score: " + player.totalScore())
+
 // export this file as a module
 module.exports = Player;
