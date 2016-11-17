f7fa049bf47a757c6d2fba493f8744cbaea41f55
laurenfb <laurenfriesbrink@gmail.com>
2016-11-17 14:24:34 -0800

    refactor if/else to ensure correct results if words are different length but same score

--- scrabble.js
+++ scrabble.js
@@ -39,7 +39,9 @@ Scrabble.highestScoreFrom = function(arrayOfWords) {
     }
     // if the scores are tied, check to see if one of the words is longer than 7 letters. if neither is, continue on as if nothing happened (this will effectively choose the first tied score to be the winner) //
     else if (scoreArray[i] == highestWordScore) {
-      if (highestWord.length < 7 && arrayOfWords[i].length >= 7) {
+      if (highestWord.length > arrayOfWords[i].length) {
+        highestWord = arrayOfWords[i];
+      } else if (highestWord.length < 7 && arrayOfWords[i].length >= 7) {
         highestWordScore = scoreArray[i]
         highestWord = arrayOfWords[i];
       };
@@ -49,24 +51,6 @@ Scrabble.highestScoreFrom = function(arrayOfWords) {
   return highestWord
 };
 
-// console.log(Scrabble.helloWorld())
-// // testing for score fx// regular word
-// console.log("Score for 'word' is: " + Scrabble.score("word"))
-// // bingo word (7+ letters)
-// console.log("Score for 'foooooooblah' is: " + Scrabble.score("foooooooblah"))
-//
-// // testing highestScoreFrom fx
-// console.log("hiii: " + Scrabble.score("hiii") + ", bloop: " + Scrabble.score("bloop") + ", cat: " + Scrabble.score("cat") )
-// console.log("highest score word is: " + Scrabble.highestScoreFrom(["hiii", "bloop", "cat"]))
-//
-// // test that in the case of an equal score, the word that is 7 letters or greater should be the winner
-// console.log("zzzzzz: " + Scrabble.score("zzzzzz") + ", iiiiiif: " + Scrabble.score("iiiiiif"))
-// console.log("highest score word is: " + Scrabble.highestScoreFrom(["zzzzzz", "iiiiiif"]))
-//
-// // test that in the case of an equal score where none is 7 letters, the first should be the winner
-// console.log("aaaaaa: " + Scrabble.score("aaaaaa") + ", iiiiii: " + Scrabble.score("iiiiii"))
-// console.log("highest score word is: " + Scrabble.highestScoreFrom(["aaaaaa", "iiiiii"]))
-// //
-//
+// hashtag test-driven development. check out /spec folder to see testing on this module.
 // export this file as a module
 module.exports = Scrabble;
